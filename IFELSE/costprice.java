a1d036244804a8cfbc31b49278f42696691b13ae
Ayushborban09 <ayushborban09@gmail.com>
2026-06-25 22:55:04 +0530

    Update DSA files

--- IFELSE/costprice.java
+++ IFELSE/costprice.java
@@ -10,13 +10,13 @@ public class costprice {
          System.out.print("Enter selling price ; ");
         int sp = sb.nextInt();
 
-      //   if (sp==cp) System.out.println("no profit");
-      //   if (cp<sp) System.out.println("profittt"+(sp-cp));
-      //   if(cp>sp) System.out.println("loss"+(cp-sp));
+        if (sp==cp) System.out.println("no profit");
+        if (cp<sp) System.out.println("profittt"+(sp-cp));
+        if(cp>sp) System.out.println("loss"+(cp-sp));
       
-      if(cp>sp) System.out.println("loss " + (cp-sp));
-      else if (cp<sp) System.out.println("profittt" + (sp-cp));
-      else  System.out.println("no profit");
+      // if(cp>sp) System.out.println("loss " + (cp-sp));
+      // else if (cp<sp) System.out.println("profittt" + (sp-cp));
+      // else  System.out.println("no profit");
 
       
          
